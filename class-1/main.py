6ed3ba12168c714a118fa0da6fa38a1f7c153da8
Muhammad Owais <165272983+Owaisi123@users.noreply.github.com>
2025-02-27 13:08:02 +0500

    Add files via upload

---
+++ class-1/main.py
@@ -0,0 +1,71 @@
+# Say! hello world in py. how is it said in python
+
+
+print("Hello World!")
+
+
+
+# Function is defined like this in python 
+
+
+
+name  = "Owais"
+
+def greet():
+    return f"hello {name}"
+print(greet())
+
+
+# data types in python                                                     Variables are made in snake case
+
+# String  for defining string type str keyword is used
+
+my_name="Muhammad Owais"
+print(my_name)
+
+
+# Number type
+
+my_age= 17
+print(my_age)
+
+
+# Float type 
+
+petrol_price=22.22
+print(petrol_price)
+
+
+# Boolean type whenever we have to get is that so or not? means true and false
+
+is_twitter_used= True
+print(is_twitter_used)
+
+
+is_insta_used=False
+print(is_insta_used)
+
+
+# list
+
+social_media_accounts_for_developers = ['Github', 'Linkedin', 'Twitter']
+print(social_media_accounts_for_developers)
+
+
+
+# tuple
+
+colors=('black','white','gray','green')
+print(colors)
+
+
+# Dictionary
+
+user_data = {"name":"Owais","class":12,"age":17,"Quarter":3}
+print(user_data)
+
+
+# Set
+
+unique_id={12,55,77,65,62,88}
+print(unique_id)
